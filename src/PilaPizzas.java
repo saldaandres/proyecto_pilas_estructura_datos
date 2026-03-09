3f085bb12ec468563c0ac8286641ab6576e8d31b
andres <saldaandres@hotmail.es>
2026-03-09 15:56:03 -0400

    Se implementan los metodos push y pop de la pila

--- src/PilaPizzas.java
+++ src/PilaPizzas.java
@@ -10,4 +10,26 @@ public class PilaPizzas {
     public boolean isEmpty() {
         return (tamano == 0);
     }
+
+    public void push(Pizza pizza) {
+        if (tope == null) {
+            tope = new Nodo(pizza, null);
+            System.out.println("Se agrega una nueva pizza a la pila");
+            tamano ++;
+            return;
+        }
+        Nodo segundo = tope;
+        tope = new Nodo(pizza, segundo);
+        tamano ++;
+    }
+
+    public Pizza pop() {
+        if (tope == null) {
+            System.out.println("La pila esta vacia. No hay objetos para retirar.");
+            return null;
+        }
+        Nodo retirado = tope;
+        tope = tope.getSiguiente();
+        return retirado.getPizza();
+    }
 }
