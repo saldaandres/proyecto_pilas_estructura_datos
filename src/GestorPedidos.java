b0c4a73b4933cee2becb8c010ddddf4e11dc32b2
andres <saldaandres@hotmail.es>
2026-03-11 19:24:30 -0400

    Se implementa el metodo registrarPizza de GestorPedidos

--- src/GestorPedidos.java
+++ src/GestorPedidos.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class GestorPedidos {
+    Scanner scanner;
+    private PilaPizzas pedidos;
+    private PilaPizzas completados;
+
+    public GestorPedidos() {
+        scanner = new Scanner(System.in);
+        pedidos = new PilaPizzas();
+        completados = new PilaPizzas();
+    }
+
+    public void registrarPizza() {
+        String nombre = scanner.next();
+        String[] ingredientes = new String[3];
+        for (int i = 0; i < ingredientes.length; i++) {
+            ingredientes[i] = scanner.next();
+        }
+        Pizza nuevaPizza = new Pizza(nombre, ingredientes);
+        pedidos.push(nuevaPizza);
+    }
+
     static void main(String[] args) {
         String nombre1 = "margarita";
         String nombre2 = "4 quesos";
